91df2c4edd58420090cc6fc7a8b1c4dfae52714a
kubaik <kevkubai@gmail.com>
2018-02-12 12:00:04 +0300

    Added code to check if Plugin is installed

--- src/main/java/com/example/mulalibrary/Mula.java
+++ src/main/java/com/example/mulalibrary/Mula.java
@@ -2,11 +2,15 @@ package com.example.mulalibrary;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Mula extends AppCompatActivity {
     Context context;
+    public String TAG = Mula.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +24,28 @@ public class Mula extends AppCompatActivity {
 
 
     public void configMula(Context context) {
-        Intent intent = new Intent(context, Main.class);
-        context.startActivity(intent);
+        boolean isPluginInstalleed = isPackageInstalled("com.plugin.consumerapp", context);
+        Log.d(TAG, "@isPluginInstalleed " + isPluginInstalleed);
+        if (isPluginInstalleed) {
+            Intent intent = new Intent(context, Main.class);
+            context.startActivity(intent);
+        } else {
+            final String appPackageName = "com.cellulant.consumerapp"; // getPackageName() from Context or Activity object
+            try {
+                context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+            } catch (android.content.ActivityNotFoundException anfe) {
+                context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+            }
+        }
 
+    }
+
+    public static boolean isPackageInstalled(String packageName, Context context) {
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            return packageManager.getApplicationInfo(packageName, 0).enabled;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
     }
 }
